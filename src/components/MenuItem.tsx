819a11650969a2aa8583add55f27ebd0d5972047
John Pablo Valverde <80083262+johnpablo7@users.noreply.github.com>
2024-05-27 23:37:31 -0500

    adding elements to the order state

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -2,13 +2,15 @@ import type { TMenuItem } from '../types';
 
 type menuItemProps = {
   item: TMenuItem;
+  addItem: (item: TMenuItem) => void;
 };
 
-export default function MenuItem({ item }: menuItemProps) {
+export default function MenuItem({ item, addItem }: menuItemProps) {
   return (
     <button
       className='border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between'
       type='button'
+      onClick={() => addItem(item)}
     >
       <p>{item.name}</p>
       <p className='font-black'>${item.price}</p>
---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import type { TMenuItem, TOrderItem } from '../types';
+
+export default function useOrder() {
+  const [order, setOrder] = useState<TOrderItem[]>([]);
+  // const [total, setTotal] = useState(0);
+  // const [auth, setAuth] = useState(false);
+
+  const addItem = (item: TMenuItem) => {
+    const itemExist = order.find((orderItem) => orderItem.id === item.id);
+    if (itemExist) {
+      console.log();
+    } else {
+      const newItem = { ...item, quantity: 1 };
+      setOrder([...order, newItem]);
+    }
+  };
+
+  console.log(order);
+
+  return {
+    addItem,
+  };
+}
