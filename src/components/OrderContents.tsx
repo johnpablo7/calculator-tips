8045f64e7f5cdbb7b564b69b43ce634be67fbd8b
John Pablo Valverde <80083262+johnpablo7@users.noreply.github.com>
2024-05-30 23:38:10 -0500

    formatting consumption and displaying quantities

--- src/components/OrderContents.tsx
+++ src/components/OrderContents.tsx
@@ -1,3 +1,4 @@
+import formatCurrency from '../helpers';
 import type { TOrderItem } from '../types';
 
 type orderContentsProps = {
@@ -9,13 +10,31 @@ export default function OrderContents({ order }: orderContentsProps) {
     <div>
       <h2 className='font-black text-4xl'>Consumo</h2>
 
-      <div>
+      <div className='space-y-3 mt-10'>
         {order.length === 0 ? (
           <p className='text-center'>La orden esta vacia</p>
         ) : (
           order.map((item) => (
-            <div key={item.id}>
-              <p>{item.name}</p>
+            <div
+              key={item.id}
+              className='flex justify-between items-center border-t border-gray-200 p-5 last-of-type:border-b'
+            >
+              <div>
+                <p className='text-lg'>
+                  {item.name} - {formatCurrency(item.price)}
+                </p>
+                <p className='font-black'>
+                  Cantidad: {item.quantity} -
+                  {formatCurrency(item.price * item.quantity)}
+                </p>
+              </div>
+
+              <button
+                className='bg-red-600 h-8 w-8 rounded-full text-white font-black'
+                type='button'
+              >
+                X
+              </button>
             </div>
           ))
         )}
